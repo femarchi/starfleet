cd477c7856df37de97d9decd55a3068146d1b43e
Felipe Passarin Marchi <marchi@esss.co>
2022-12-18 20:12:40 -0300

    Enable navigation through dashboard buttons

--- catalog/src/app/dashboard/dashboard.component.ts
+++ catalog/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StarfleetRoutes } from '../starfleet/starfleet-routes';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  template: `
+    <div class="dashboard-wrapper">
+      <div class="dashboard-content p-grid">
+        <div class="p-col-12 dashboard-title">
+          <h1>Welcome to the Star Wars Catalog</h1>
+        </div>
+        <div class="p-col-12 dashboard-buttons">
+          <p-button label="Ships" (onClick)="navigateToShips()"></p-button>
+          <p-button label="Pilots" (onClick)="navigateToPilots()"></p-button>
+        </div>
+      </div>
+    </div>
+  `,
 })
 export class DashboardComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
+  public navigateToShips(): void {
+    this.router.navigate(['..', StarfleetRoutes.Ships], { relativeTo: this.route });
+  }
+
+  public navigateToPilots(): void {
+    this.router.navigate(['..', StarfleetRoutes.Pilots], { relativeTo: this.route });
+  }
 }
